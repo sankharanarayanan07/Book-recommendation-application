5a2d235ab43673475a8d42d21b15e1fd2a6068d8
sankharanarayanan07 <sankharworld@gmail.com>
2025-05-18 16:03:04 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
